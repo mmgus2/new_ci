2017be5fcb4ce6b6ef50eccca5830b7b1545be07
Gus <mmgus2@student.monash.edu>
2016-04-13 04:12:37 +1000

    add files and changes

---
+++ application/views/coming_soon-view.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gusis
+ * Date: 4/13/2016
+ * Time: 4:03 AM
+ */
+?>
+<section id="team" class="bg-light-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Coming Soon</h2>
+                <h3 class="section-subheading text-muted">Wait For it.</h3>
+            </div>
+        </div>
+    </div>
+</section>
