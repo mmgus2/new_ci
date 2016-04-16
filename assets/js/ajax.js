fb932be8313f0f442ad5a696449be93594b39263
Gus <mmgus2@student.monash.edu>
2016-04-16 23:12:55 +1000

    changes

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,8 +3,8 @@
  */
 $(document).ready(function() {
     //initialise base url for ajax request
-    //var url = window.location.protocol + "//" + window.location.host + "/Ajax/";
-    var url = "http://localhost/index.php/Ajax/";
+    var url = window.location.protocol + "//" + window.location.host + "/Ajax/";
+    //var url = "http://localhost/index.php/Ajax/";
 
     //variables to store forest data
     var forestData = [];
