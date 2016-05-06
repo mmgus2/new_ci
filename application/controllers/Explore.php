16333ad4f77dd58b74baa23007191bbc84f2380a
Gus <mmgus2@student.monash.edu>
2016-05-06 15:56:56 +1000

    change

--- application/controllers/Explore.php
+++ application/controllers/Explore.php
@@ -7,11 +7,24 @@
  */
 class Explore extends CI_Controller {
 
+    private $record = null;
+    private $model = null;
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Database_model');
+        $this->model = $this->Database_model;
+        $this->load->helper('geodistance');
+    }
+
     public function index()
     {
+        $this->record = $this->model->read_activity();
+        $data['activity'] = $this->record;
+
         $this->load->view('header');
         $this->load->view('menu');
-        $this->load->view('explore-view');
+        $this->load->view('explore-view','$data');
         $this->load->view('footer');
     }
 }
--- application/views/explore-view.php
+++ application/views/explore-view.php
@@ -17,6 +17,8 @@
 </header>
 <section class="bg-light-gray">
     <div class="container">
+        <input type="hidden" id="max_distance" />
+        <input type="hidden" id="display_mode" value="distance" />
         <div class="row">
             <div class="col-sm-12 text-left">
                 <h3 class="section-subheading text-muted">Check out our website to know about the victorian forests.</h3>
@@ -30,6 +32,33 @@
                 <div class="col-sm-12">
                     <input class="form-control" id="input_loc" placeholder="Specify your location" type="text">
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-10">
+                    <br />
+                    <input type="range" min="0" step="1" id="range_slider">&nbsp;
+                    <br />
+                </div>
+                <div class="col-sm-2">
+                    <select class="form-control" id="unit">
+                        <option value="K" selected>Km</option>
+                        <option value="M">Mile</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <?php
+                    for ($i = 0; $i < sizeof($activity); $i++){
+                        ?>
+                        <div class="col-sm-1">
+                            <img src="../../assets/img/buttons/'<?php echo $activity[$i]["activity_id"] ?>.png"
+                                 alt="<?php echo $activity[$i]["activity_name"] ?>"
+                                 class="img-responsive" id="<?php echo $activity[$i]["activity_id"] ?>"
+                                 <p><?php echo $activity[$i]["activity_name"] ?></p>
+                        </div>
+                <?php
+                    }
+                ?>
             </div>
         </div>
     </div>
