ff2621aa23d9b77e31f899ed337e1ac1477d9784
Gus <mmgus2@student.monash.edu>
2016-05-06 16:30:08 +1000

    change

--- application/views/explore-view.php
+++ application/views/explore-view.php
@@ -25,17 +25,21 @@
             </div>
         </div-->
         <div class="panel panel-default" id="menu_container">
-            <div class="row" style="margin: 0 10px;">
-                <div class="col-sm-12">
+            <div class="row" style="margin: 10px 0;">
+                <div class="col-sm-10 col-sm-offset-1">
                     <div id="input_info" hidden="true"></div>
                 </div>
-                <div class="col-sm-12">
+                <div class="col-sm-10 col-sm-offset-1">
                     <input class="form-control" id="input_loc" placeholder="Specify your location" type="text">
                 </div>
             </div>
-            <hr  style="margin: 0 10px;" />
             <div class="row">
-                <div class="col-sm-10"  style="margin: 0 10px;">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <hr />
+                </div>
+            </div>
+            <div class="row" style="margin: 10px 0;">
+                <div class="col-sm-8 col-sm-offset-1" >
                     <br />
                     <input type="range" min="0" step="1" id="range_slider">&nbsp;
                     <br />
@@ -49,8 +53,12 @@
                     <br />
                 </div>
             </div>
-            <hr  style="margin: 0 10px;" />
-            <div class="row row-centered"  style="margin: 0 10px;">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <hr />
+                </div>
+            </div>
+            <div class="row row-centered" style="margin: 10px 0;">
                 <?php
                     for ($i = 0; $i < sizeof($activity); $i++){
                         ?>
