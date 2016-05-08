a4ffb26e1494bc5e08e15e16c5f3605d071630d6
Gus <mmgus2@student.monash.edu>
2016-05-08 22:51:45 +1000

    change

--- application/views/explore-view.php
+++ application/views/explore-view.php
@@ -108,7 +108,7 @@
                     <td>
                         <img src="../../assets/img/buttons/<?php echo $activity[$i]["activity_id"] ?>.png"
                              alt="<?php echo $activity[$i]["activity_name"] ?>"
-                             class="img-responsive" id="<?php echo $activity[$i]["activity_id"] ?>"
+                             class="img-responsive img-button" id="<?php echo $activity[$i]["activity_id"] ?>"
                              onclick="acceptActivity(this)"
                         >
                     </td>
