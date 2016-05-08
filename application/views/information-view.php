aa51f925164a87b479eab3cf74d9f92b0d0072b8
Gus <mmgus2@student.monash.edu>
2016-05-08 19:58:55 +1000

    change

--- application/views/information-view.php
+++ application/views/information-view.php
@@ -24,6 +24,7 @@
                 for ($i=0; $i < sizeof($allforests); $i++) {
                 ?>
                     <li>
+                        <div class="col-sm-12 col-md-6">
                         <div class="col-sm-6 col-md-3">
                             <img  src="<?php echo base_url() ?>assets/img/forest_images/<?php echo $allforests[$i]['id'] ?>.png"
                                   class="img-responsive" alt="<?php echo $allforests[$i]['name'] ?>" >
@@ -50,6 +51,7 @@
                                    role="button" download>Review &raquo;
                                 </a>
                             </p>
+                        </div>
                         </div>
                     </li>
                     <hr>
