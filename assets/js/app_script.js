f53f4266cc02073fedd061e444b157d378fe449e
Gus <mmgus2@student.monash.edu>
2016-05-08 21:04:11 +1000

    change

--- assets/js/app_script.js
+++ assets/js/app_script.js
@@ -659,13 +659,13 @@ $(document).ready(function() {
             }
 
             if (type == 'site'){
-                /*popupInfo += '<p><a href="http://maps.google.com/maps?saddr=' + userLatitude + ',' + userLongitude +
+                popupInfo += '<p><a href="http://maps.google.com/maps?saddr=' + userLatitude + ',' + userLongitude +
                     '&daddr=' +  data[i].latitude + ',' +  data[i].longitude +
-                    '" target="_blank" class="btn btn-success">Get direction</a></p>'*/
-                popupInfo += '<p><button class="btn btn-success" ' +
+                    '" target="_blank" class="btn btn-success">Get direction</a></p>'
+                /*popupInfo += '<p><button class="btn btn-success" ' +
                     'onclick="getDirection(' + userLatitude + ',' + userLongitude + ',' +
                                             data[i].latitude + ',' +  data[i].longitude +')">' +
-                    'Get Direction</button></p>';
+                    'Get Direction</button></p>';*/
             }
 
             var marker_link = '';
