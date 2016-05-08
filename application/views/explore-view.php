e7de39eb35c2f65015dc78435605801cb751d62d
Gus <mmgus2@student.monash.edu>
2016-05-08 22:07:21 +1000

    change

--- application/views/explore-view.php
+++ application/views/explore-view.php
@@ -171,8 +171,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="fa-border" id="map" style="width: 100%; height: 600px;">
-                    <img src="../../assets/img/ajax-loader.gif" style="display:block; margin:auto auto;" />
+                <div class="fa-border" id="map"
+                     style="width: 100%; height: 600px; background:url(../../assets/img/ajax-loader.gif) no-repeat center center;">
+                    <!--img src="../../assets/img/ajax-loader.gif" style="display:block; margin:auto auto;" /-->
                 </div>
             </div>
         </div>
