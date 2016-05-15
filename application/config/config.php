d0ad259905a4ea5587b9bb776cec0de4b796c711
Gus <mmgus2@student.monash.edu>
2016-05-16 09:28:54 +1000

    change

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = '';
-$config['base_url'] = 'http://xploreforest-development.azurewebsites.net/';
+//$config['base_url'] = 'http://xploreforest-development.azurewebsites.net/';
+$config['base_url'] = 'http://xploreforest.azurewebsites.net/';
 //$config['base_url'] = 'http://localhost/index.php/';
 /*
 |--------------------------------------------------------------------------
